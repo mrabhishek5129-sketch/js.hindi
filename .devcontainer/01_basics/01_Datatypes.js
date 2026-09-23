557c2c5fbd6b0b7b1388e3fab45e9a2c57b57465
Abhishek <mrabhishek5129@gmail.com>
2026-09-23 16:37:21 +0000

    Learn JavaScript variables and datatypes

---
+++ .devcontainer/01_basics/01_Datatypes.js
@@ -0,0 +1,7 @@
+"use strict"; // treat all js code never version
+
+//alert(3+3);// we are  using node.js, not browser
+
+console.log(3+3);
+
+console.log("Abhishek")
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountID=144553
+let accountEmail= "abhishek@456"
+var accountPassword ="12345"
+AccountCity ="bhadohi"
+
+// accountID= 2// not allowd
+
+accountEmail="abhishek@456"
+accountPassword ="12345"
+AccountCity ="bhadohi"
+
+console.log(accountID);
+console.table([accountEmail,accountPassword,AccountCity]);
