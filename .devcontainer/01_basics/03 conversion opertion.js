bee4750d539a43b8f394018ca48e7adf44806232
Abhishek <mrabhishek5129@gmail.com>
2026-09-26 07:08:33 +0000

    coversion

---
+++ .devcontainer/01_basics/03 conversion opertion.js
@@ -0,0 +1,22 @@
+let scroe =true
+ 
+console.log(typeof scroe);
+console.log(typeof(scroe));
+
+let valueInNumber=Number(scroe)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn="1"
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
+
+let someNumber = 55;
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
+
